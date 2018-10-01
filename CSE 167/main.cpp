07bba946992b5529c0de07c079bcb054b262f657
Dai David <David.Dai@ljcds.irg>
2018-10-01 10:33:35 -0700

    Initial Commit

---
+++ CSE 167/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CSE 167
+//
+//  Created by Dwei on 10/1/18.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
